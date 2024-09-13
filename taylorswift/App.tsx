fc74067a8d5147fed16207623aed226266d962fd
rayssamsc <160240149+rayssamsc@users.noreply.github.com>
2024-09-13 10:18:07 -0300

    1309

--- taylorswift/App.tsx
+++ taylorswift/App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>my name</Text>
-      <StatusBar style="auto" />
+      <View style={styles.sue}>
+        <text>sue</text>
+      </View>
+      <View style={styles.taylorswift}>
+        <text>taylorswift</text>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  sue: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+    
+
+  },
+  taylorswift:  {
+    width: 100,
+    height: 100,
+    backgroundColor: 'lightcoral',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
   },
 });
